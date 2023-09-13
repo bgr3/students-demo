b03302f4aed7c8830b12d599273aa2df554fa2a8
Alexey Borisenko <bgr666@gmail.com>
2023-09-13 19:50:06 +0300

    lesson1

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
+const products = [{title: 'tomato'}, {title: 'orange'}];
+const adress = [{value: 'srlmash'}, {value: 'volotova'}];
+
 app.get('/', (req: Request, res: Response) => {
   let hello = 'Hello 123';
   res.send(hello);
+})
+
+app.get('/products', (req: Request, res: Response) => {
+  res.send(products)
 })
 
 app.listen(port, () => {
