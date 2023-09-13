43285c93a10f306d6c4fed55125ce49055ebb69a
Alexey Borisenko <bgr666@gmail.com>
2023-09-13 19:33:22 +0300

    heroku back

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-  let hello = 'Hello :)';
+  let hello = 'Hello 123';
   res.send(hello);
 })
 
