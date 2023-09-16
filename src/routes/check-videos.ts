a7854b87ca77cba5ddafc03d3352cce71bde6e39
Alexey Borisenko <bgr666@gmail.com>
2023-09-16 23:18:30 +0300

    homework4

--- src/routes/check-videos.ts
+++ src/routes/check-videos.ts
@@ -11,19 +11,19 @@ export let checkVideos = function (req:any) {
     if (req.body.title.length > 40) {
       let err1 = new Messages;
       err1.message = "Title too long!";
-      err1.field = req.body.title;
+      err1.field = "title";
       errors.errorsMessages.push(err1);
     } else if (req.body.title.length < 1) {
       let err2 = new Messages;
       err2.message = "Title should not be empty!";
-      err2.field = req.body.title;
+      err2.field = "title";
       errors.errorsMessages.push(err2);
     }
 
     if (typeof req.body.title != 'string') {
       let err3 = new Messages;
       err3.message = "Title must be string!";
-      err3.field = req.body.title;
+      err3.field = "title";
       errors.errorsMessages.push(err3);
     }  
   } else {
@@ -37,19 +37,19 @@ export let checkVideos = function (req:any) {
     if (req.body.author.length > 40) {
       let err4 = new Messages;
       err4.message = "Author too long!";
-      err4.field = req.body.author;
+      err4.field = "author";
       errors.errorsMessages.push(err4);
     } else if (req.body.author.length == 0) {
       let err5 = new Messages;
       err5.message = "Author should not be empty!";
-      err5.field = req.body.author;
+      err5.field = "author";
       errors.errorsMessages.push(err5);
     }
 
     if (typeof req.body.author != 'string') {
       let err6 = new Messages;
       err6.message = "Author must be string!";
-      err6.field = req.body.author;
+      err6.field = "author";
       errors.errorsMessages.push(err6);
     }  
   } else {
@@ -62,14 +62,14 @@ export let checkVideos = function (req:any) {
   if (req.body.availableResolutions.length == 0) {
     let err7 = new Messages;
     err7.message = "No avaliable resolutions!";
-    err7.field = req.body.availableResolutions;
+    err7.field = "availableResolutions";
     errors.errorsMessages.push(err7);
   }
 
   if (!(req.body.availableResolutions.every((i: string) => availableResolutions.includes(i)))) {
     let err8 = new Messages;
     err8.message = "No match resolutions!";
-    err8.field = `${req.body.availableResolutions} not exist! Avaliable resolutions ${availableResolutions}`;
+    err8.field = "availableResolution";
     errors.errorsMessages.push(err8);
   }
 
@@ -77,7 +77,7 @@ export let checkVideos = function (req:any) {
     if (typeof req.body.canBeDownloaded != 'boolean'){
       let err9 = new Messages;
       err9.message = "Avaliable 'true' or 'false'!";
-      err9.field = req.body.canBeDownloaded;
+      err9.field = "canBeDownloaded";
       errors.errorsMessages.push(err9);
     }
   }
@@ -86,7 +86,7 @@ export let checkVideos = function (req:any) {
     if (req.body.minAgeRestriction < 1 || req.body.minAgeRestriction > 18){
       let err10 = new Messages;
       err10.message = "must be between 1 and 18";
-      err10.field = req.body.minAgeRestriction;
+      err10.field = "minAgeRestriction";
       errors.errorsMessages.push(err10);
     }
   }
@@ -95,7 +95,7 @@ export let checkVideos = function (req:any) {
     if (typeof req.body.publicationDate != typeof new Date().toDateString()){
       let err11 = new Messages;
       err11.message = "Date not exist";
-      err11.field = req.body.publicationDate;
+      err11.field = "publicationDate";
       errors.errorsMessages.push(err11);
     }
   }
