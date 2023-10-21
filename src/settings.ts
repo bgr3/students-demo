a0ab2ef999ad61b4ae3bd2b8d3fe6f63aceb85ec
Alexey Borisenko <bgr666@gmail.com>
2023-10-21 20:38:27 +0300

    hw6_0.1

--- src/settings.ts
+++ src/settings.ts
@@ -33,7 +33,7 @@ export const RouterPaths = {
 
 app.use(express.json())
 
-//app.use(authGuardMiddleware)
+//app.use(authGuardMiddleware) 
 
 app.get('/', (req: Request, res: Response) => {
   let hello = `Hello!`;
